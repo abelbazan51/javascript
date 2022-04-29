df03e49238f47e3ff40c964cf1a0f111ade71140
abel bazan <abelbazan51@gmail.com>
2022-04-29 20:58:37 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+//esto es un comentario //
+/*comentario en varias lineas */
+//ES6
+//let numeroUno;
+
+//numeroUno = 1;
+//numeroUno = "56";
+
+//let nombre = "maxi";
+
+//const PI = 3.141516;
+
+//let nombreUsuario = prompt("ingresa tu nombre");
+
+//alert("hola " + nombreUsuario);
+
+//console.log("hola " + nombreUsuario);
+
+//let nota = prompt("cuanto es 3+3");
+
+//console.log(nota >= 6);
+
+//if (nota == 6) {
+// console.log("bien");
+//} else {
+//  console.log("falso");
+//}
+
+//let disparo = prompt("cuanto es 2x2");
+//console.log(nota >= 4);
+//if (disparo == 4) {
+//console.log("correcto");
+//} else {
+// console.log("equibocado");
+//}
+
+//let numero = prompt("cuanto es 9+9");
+//console.log(nota <= 18);
+//if (numero == 18) {
+//console.log("correcto");
+//} else {
+//console.log("incorrecto");
+//}
+
+let uno = prompt("juguemos a la batalla naval :) cuanto es 2x2 ??");
+
+if (uno == 4) {
+  console.log("me diste");
+  alert("me diste ");
+} else {
+  console.log("fallaste !! aguaaaa!!!");
+  alert("fallaste !!!aguaaaa!!!");
+}
+
+let dos = prompt(" cuanto es 9+9???");
+
+if (dos == 18) {
+  console.log("awwwch me diste ");
+  alert("aaawwchhhh me diste  ");
+} else {
+  console.log("aguaaa!!! alguien no termino la escuela jaja");
+  alert("aguaaa!!! alguien no termino la escuela jaja");
+}
+
+switch (uno + dos) {
+  case 1:
+    console.log("jaja 1? ");
+    break;
+
+  default:
+    console.log("ganaste ");
+    break;
+}
